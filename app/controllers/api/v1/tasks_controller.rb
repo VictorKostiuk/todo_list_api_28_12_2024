46e9e786742887def8dc6d621eafecc1f4156a75
ViktorKostiuk <vityok.kostiuk@gmail.com>
2024-12-28 14:27:39 +0200

    Added tasks controller

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,62 @@
+class Api::V1::TasksController < ApplicationController
+  before_action :set_task, only: %i[ show update destroy ]
+  before_action :set_list, only: %i[ index create ]
+
+  # GET /tasks
+  def index
+    if @list
+      @tasks = @list.tasks
+    else
+      @tasks = Task.all
+    end
+
+    render json: @tasks
+  end
+
+  # GET /tasks/1
+  def show
+    render json: @task
+  end
+
+  # POST /tasks
+  def create
+    @task = Task.new( @list ? task_params.merge({list_id: @list.id}) : task_params)
+
+    if @task.save
+      render json: @task, status: :created
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /tasks/1
+  def update
+    if @task.update(task_params)
+      render json: @task
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /tasks/1
+  def destroy
+    @task.destroy!
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_list
+      if params[:list_id]
+        @list = List.find(params[:list_id])
+      end
+    end
+
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    # Only allow a task of trusted parameters through.
+    def task_params
+      params.require(:task).permit(:name, :description, :deadline, :list_id)
+    end
+end
